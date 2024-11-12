7b3fa29d76ee2756df5c549342a699fe7a7fef23
AwanArdy <muhammadawan46@gmail.com>
2024-11-12 09:48:42 +0800

    update db and models student

---
+++ backend/models/Student.js
@@ -0,0 +1,54 @@
+const pool = require('../config/db');
+
+const Student = {
+  getAll: async (callback) => {
+    try {
+      const res = await pool.query('SELECT * FROM students');
+      callback(null, res.rows);
+    } catch (err) {
+      callback(err, null);
+    }
+  },
+  getById: async (id, callback) => {
+    try {
+      const res = await pool.query('SELECT * FROM students WHERE id = $1', [id]);
+      callback(null, res.rows[0]);
+    } catch (err) {
+      callback(err, null);
+    }
+  },
+  create: async (data, callback) => {
+    const { name, age } = data;
+    try {
+      const res = await pool.query(
+        'INSERT INTO students (name, age) VALUES ($1, $2) RETURNING *',
+        [name, age]
+      );
+      callback(null, res.rows[0]);
+    } catch (err) {
+      callback(err, null);
+    }
+  },
+  update: async (id, data, callback) => {
+    const { name, age } = data;
+    try {
+      await pool.query(
+        'UPDATE students SET name = $1, age = $2 WHERE id = $3',
+        [name, age, id]
+      );
+      callback(err);
+    } catch (err) {
+      callback(err);
+    }
+  },
+  delete: async (id, callback) => {
+    try {
+      await pool.query('DELETE FROM students WHERE id = $1', [id]);
+      callback(null);
+    } catch (err) {
+      callback(err);
+    }
+  }
+};
+
+module.exports = Student;
